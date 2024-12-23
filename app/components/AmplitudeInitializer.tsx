098274bd05b109f815edb13f1cf72b060b53c64e
Matt Phelps <matt@leandme.com>
2024-12-23 22:19:44 +0100

    amplitude

--- app/components/AmplitudeInitializer.tsx
+++ app/components/AmplitudeInitializer.tsx
@@ -9,19 +9,20 @@ const AmplitudeInitializer = () => {
   useEffect(() => {
     if (AMPLITUDE_API_KEY) {
       amplitude.init(AMPLITUDE_API_KEY, {
+        serverUrl: "https://api.eu.amplitude.com", // EU server zone
         autocapture: {
           elementInteractions: true,
           formInteractions: false,
           fileDownloads: false,
         },
       });
-      console.log("Amplitude initialized");
+      console.log("Amplitude initialized with EU server zone");
     } else {
       console.error("Missing Amplitude API Key");
     }
   }, []);
 
-  return null; // This component only initializes Amplitude and doesn't render anything
+  return null;
 };
 
 export default AmplitudeInitializer;
