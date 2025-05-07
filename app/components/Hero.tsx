00b55d1c75d98ac0aacc2f791b1ffcf2ecff3c02
Matt Phelps <matt@leandme.com>
2025-05-07 13:07:28 +0200

    fine

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,27 +1,37 @@
 import ReviewBox from "./ReviewBox";
-
+import { trackEvent } from "../libs/amplitude";
 
 export default function Hero() {
-    return (
-      <div className="hero min-h-screen text-white bg-background lg:-mt-28 -mt-48 flex items-center justify-center">
-        <div className="hero-content text-center">
-          <div className="max-w-2xl "> 
+  const handleClick = () => {
+     trackEvent("Go to Checkout", {
+                  location: "hero cta",
+                });
+    window.location.href = "https://buy.stripe.com/28o8xOevc801a9W6oq"; // Redirect manually
+  };
+
+  return (
+    <div className="hero min-h-screen text-white bg-background lg:-mt-28 -mt-48 flex items-center justify-center">
+      <div className="hero-content text-center">
+        <div className="max-w-2xl">
           <ReviewBox />
           <h1 className="text-3xl mt-6 lg:mt-10 lg:text-5xl font-bold">
             The Proven 17 Steps to Get Your Best Jawline
           </h1>
-            <p className="py-6 text-md lg:text-lg text-lightgray -mb-6 -mt-0 lg:mt-6">
+          <p className="py-6 text-md lg:text-lg text-lightgray -mb-6 -mt-0 lg:mt-6">
             The step-by-step jawline formula for men who want to improve their jawline naturally and become more attractive.
-            </p>
-            <p className="py-6 text-2xl font-bold -mb-6 lg:text-3xl text-primary">
+          </p>
+          <p className="py-6 text-2xl font-bold -mb-6 lg:text-3xl text-primary">
             $17
-            </p>
-            <a href="https://buy.stripe.com/28o8xOevc801a9W6oq">
-              <button className="btn btn-primary btn-lg rounded-full text-white mt-6">CLICK HERE TO GET THE FORMULA <span className="text-lg">→</span></button>
-            </a>
-          </div>
+          </p>
+
+          <button
+            onClick={handleClick}
+            className="btn btn-primary btn-lg rounded-full text-white mt-6"
+          >
+            CLICK HERE TO GET THE FORMULA <span className="text-lg">→</span>
+          </button>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
