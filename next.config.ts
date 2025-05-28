d4fceebb4aa37aece0c8b5daf8b8c5ac6fef662a
Matt Phelps <matt@leandme.com>
2025-05-28 23:02:54 +0200

    added the urls

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,66 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/a",
+        destination: "/?utm_source=youtube&utm_campaign=tighten_your_jawline_mpr",
+        permanent: true,
+      },
+      {
+        source: "/b",
+        destination: "/?utm_source=youtube&utm_campaign=one_exercise_unlocks_mewing",
+        permanent: true,
+      },
+      {
+        source: "/c",
+        destination: "/?utm_source=youtube&utm_campaign=debloat_your_face",
+        permanent: true,
+      },
+      {
+        source: "/d",
+        destination: "/?utm_source=youtube&utm_campaign=get_a_deep_voice",
+        permanent: true,
+      },
+      {
+        source: "/e",
+        destination: "/?utm_source=youtube&utm_campaign=fix_asymmetrical_face_and_jaw",
+        permanent: true,
+      },
+      {
+        source: "/f",
+        destination: "/?utm_source=youtube&utm_campaign=how_to_get_a_chiseled_jawline",
+        permanent: true,
+      },
+      {
+        source: "/g",
+        destination: "/?utm_source=youtube&utm_campaign=10_mewing_mistakes_uglier",
+        permanent: true,
+      },
+      {
+        source: "/h",
+        destination: "/?utm_source=youtube&utm_campaign=how_to_mew_in_5_steps",
+        permanent: true,
+      },
+      {
+        source: "/i",
+        destination: "/?utm_source=youtube&utm_campaign=how_to_do_mewing_tongue_posture",
+        permanent: true,
+      },
+      {
+        source: "/j",
+        destination: "/?utm_source=youtube&utm_campaign=2_ways_to_know_your_mewing_correctly",
+        permanent: true,
+      },
+      {
+        source: "/k",
+        destination: "/?utm_source=youtube&utm_campaign=daily_routine_get_you_lean",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
